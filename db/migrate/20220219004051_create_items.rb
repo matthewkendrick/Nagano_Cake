a5be6d31c97338f4ffb3b13335b2025e714cb810
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-19 09:24:58 +0000

    [Edit]: Migrate files
    Message body
    Edit and prepare the admin and customer files for migration.
    
    Message footer
    Applied_Knowledge_Phase

--- db/migrate/20220219004051_create_items.rb
+++ db/migrate/20220219004051_create_items.rb
@@ -2,6 +2,13 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
 
+      t.integer :genre_id,   null: false, default: ""
+      t.string  :name,       null: false, default: ""
+      t.string  :image_id,   null: false, default: ""
+      t.text    :detail,     null: false, default: ""
+      t.integer :price,      null: false, default: ""
+      t.boolean :is_active,  null: false, default: true
+
       t.timestamps
     end
   end
