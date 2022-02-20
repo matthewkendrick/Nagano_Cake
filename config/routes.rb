c1ba2a95adde931ed7561554844b1903126c61da
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-20 20:52:13 +0000

    [Edit]: Routes
    Message body
    Edit the rest of route so that the device can be used.
    
    Message footer
    Applied_Knowledge_Phase

--- config/routes.rb
+++ config/routes.rb
@@ -1,87 +1,46 @@
 Rails.application.routes.draw do
 
   devise_for :admin, skip: [:regidtration, :passwords], controllers: {
-    sessions: 'admin/sessions'
+    sessions:      'admin/sessions'
+    registrations: 'admin/registrations'
   }
 
+  devise_for :customers, skip: [:passwords], controllers: {
+    sessions:       'public/sessions',
+    registrations:  'public/registrations'
+  }
+
+  root to:      => 'public/homes#top' 
+  get '/about'  => 'public/homes#about'
+  get '/admin'  => 'admin/homes#top'
+
+
   namespace :admin do
-    root to: 'homes#top'
-    resources :products,      expect: [:destroy]
+    root to: 'homes#top', as: 'top'
+    
+    resources :items,         expect: [:destroy]
     resources :genres,        only:   [:index, :edit, :create, :update]
     resources :customers,     only:   [:index, :show, :edit,   :update]
     resources :orders,        only:   [:index, :show, :update]
     resources :order_details, only:   [:update]
   end
 
-  root to: "public/homes#top" 
+  scope module: :public do
+    get     '/about'           => 'homes#about'
+    get     '/customers/quit'  => 'customers#quit'
+    patch   '/customers/out'   => 'customers#out'
+    delete  '/cart_items'      => 'cart_items#destroy_all'
 
-  devise_for :customers, skip: [:passwords], controllers: {
-    sessions:       'public/sessions',
-    registrations:  'public/registrations'
-  }
-TODO(ここからルートの記述変更を再開する)
+    resources :items,       only:   [:index, :show]
+    resources :customers,   only:   [:show,  :edit,   :update]
+    resources :deliveries,  expect: [:new, :show]
+    resources :orders,      expect: [:edit,  :update, :destroy] do
+      collection do
+        get   'thanks'
+        post  'log'
+      end
+    end
+  end
 
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/log'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/quit'
-    get 'customers/out'
-  end
-  namespace :public do
-    get 'cart_items/create'
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :customers
-  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
