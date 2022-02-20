9983b86fbde00f2de4b448c32c84378af8715eef
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-20 03:46:06 +0000

    [Edit]: Routes
    Message body
    Edit the route so that the device can be used.
    
    Message footer
    Applied_Knowledge_Phase

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,7 +9,7 @@ class Admin::SessionsController < Devise::SessionsController
   end
 
   def aftere_sign_out_path_for(resource)
-    admin_session_path
+    new_admin_session_path
   end
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  devise_for :admin, controllers: {
+    sessions:       'admin/sessions',
+    passwords:      'admin/passwords',
+    registrations:  'admin/registrations'
+  }
+
+  devise_for :customers, controllers: {
+    sessions:       'public/sessions',
+    passwords:      'public/passwords',
+    registrations:  'public/registrations'
+  }
+
   namespace :admin do
     get 'order_details/update'
   end
