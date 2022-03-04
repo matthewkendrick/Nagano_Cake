360f0e5f99427f6db051ab4d3bf6f95e8b860a46
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-03-04 20:26:22 +0000

    [Fix]: Routes
    Message body
    Fixed a bug that prevented customers from going to the top page when they tried log in.
    
    Message footer
    Applied_Knowledge_Phase

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -13,9 +13,9 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -25,7 +25,7 @@ class Public::SessionsController < Devise::SessionsController
   protected
 
   def active_customer?
-    @customer = Customer.find(email: params[:customer][:email])
+    @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
     if (@customer.valid_password?(params[:customer][:password])) && (@customer.is_deleted == true)
       flash[:notice] = "アカウントが退会済みです。新規登録を行なってください。"
